8ae07b5c69067d69f8ee129c581919804afba364
16kajal7@gmail.com <16kajal7@gmail.com>
2025-09-09 20:58:17 +0530

    Celsius to Fahrenheit

---
+++ convertCelsiusToFarenheit.py
@@ -0,0 +1,6 @@
+# This is a Python program that asks the user to enter a temperature in Celsius
+# and converts it to Fahrenheit.
+
+N = float(input("Enter a temperate in Celsius: "))
+F = N * 9/5 + 32
+print(f"Celsius {N} is equal to {F} in Fahrenheit")
