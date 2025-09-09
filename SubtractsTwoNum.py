127cec718caa1f23fd401697aa344943f1293ad1
16kajal7@gmail.com <16kajal7@gmail.com>
2025-09-09 20:28:43 +0530

    Subtract second number from first

---
+++ SubtractsTwoNum.py
@@ -0,0 +1,8 @@
+#This program subtracts the second number from the first number
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+subtract = num1 - num2
+
+print("The subtraction of first and second number is: ",subtract)
