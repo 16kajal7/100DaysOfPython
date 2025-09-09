bc3fe91c80ea4f01034306c001c2a82b867a6050
16kajal7@gmail.com <16kajal7@gmail.com>
2025-09-09 20:34:03 +0530

    Finding larger among two numbers

---
+++ findLargerNum.py
@@ -0,0 +1,9 @@
+#This program finds the larger number among the two numbers
+
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
+if num1 > num2:
+    print("The first number is larger than second")
+else :
+    print("The second number is larger than first")
