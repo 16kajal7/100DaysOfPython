bdf86965d0834b01abc84db177563f4285f6988b
16kajal7@gmail.com <16kajal7@gmail.com>
2025-09-11 12:38:40 +0530

    Not Operator

---
+++ notOperator.py
@@ -0,0 +1,8 @@
+#This program finds whether the person has good credit and doesn't
+# have a criminal record then they are eligible for loan
+
+g_credit = True
+has_criminal_record = False
+
+if g_credit and not has_criminal_record:
+    print("Eligible for loan")
