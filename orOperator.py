61150c03c8782312a2137cd367b0a85a252e68b8
16kajal7@gmail.com <16kajal7@gmail.com>
2025-09-11 12:33:52 +0530

    or operator

---
+++ orOperator.py
@@ -0,0 +1,9 @@
+# This python program checks if the person has high income or good credit
+# then it will print that the person is eligible for loan
+
+
+high_income = True
+good_credit = False
+
+if high_income or good_credit:
+    print("Eligible for loan")
