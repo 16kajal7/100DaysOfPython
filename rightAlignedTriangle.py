4e3c949b67cab6dca8b551b6e1c41ed4131a0830
16kajal7@gmail.com <16kajal7@gmail.com>
2025-09-24 10:16:22 +0530

    right aligned star triangle pattern full file

--- rightAlignedTriangle.py
+++ rightAlignedTriangle.py
@@ -1,1 +1,6 @@
 #This python program prints right aligned *(star) triangle pattern
+
+rows = int(input("Enter number of rows: "))
+
+for i in range(1, rows + 1):
+    print(" " * (rows-i) + '*' * i)
