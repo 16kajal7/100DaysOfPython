816b8a00be5a46d340ae9ebafc0c9ef87e655820
16kajal7@gmail.com <16kajal7@gmail.com>
2025-10-01 19:59:14 +0530

    Print Sum of numbers in a List, Print all even and odd numbers from 1 to 20

---
+++ SumOfNumInList.py
@@ -0,0 +1,9 @@
+#This python program calculates the sum of numbers in a List
+
+numbers = [1,2,3,4,5]
+total = 0
+
+for num in numbers:
+    total = total + num
+
+print(total)
---
+++ printEvenNumbers.py
@@ -0,0 +1,5 @@
+#This python program prints even numbers from 1 to 20
+
+for i in range (1, 21):
+    if i % 2 == 0:
+        print(i)
